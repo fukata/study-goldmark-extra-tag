a5c3cbb6cc43dbee33c32fac7297eee7652b7de9
fukata <tatsuya.fukata@gmail.com>
2022-07-18 09:09:29 +0900

    ディレクトリ構成を変更

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"github.com/fukata/study-goldmark-extra-tag/custom"
+	customParser "github.com/fukata/study-goldmark-extra-tag/custom/parser"
+	customHtml "github.com/fukata/study-goldmark-extra-tag/custom/renderer/html"
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark/parser"
 )
@@ -20,17 +21,18 @@ message2
 
 message3
 
-# Custom Image
+# CustomExtension Image
 
 [image id="1" caption="TEST"]
 
 `)
 
 	md := goldmark.New(
-		goldmark.WithExtensions(custom.Custom),
+		goldmark.WithExtensions(customParser.CustomExtension),
 		goldmark.WithParserOptions(
 			parser.WithAutoHeadingID(),
 		),
+		goldmark.WithRenderer(customHtml.NewCustomRenderer()),
 	)
 
 	var buf bytes.Buffer
